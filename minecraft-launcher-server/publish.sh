4f0f85d681a2101ac8fc337001cb4bb7a9c736c1
Timerix22 <timerix11@gmail.com>
2024-01-06 22:44:06 +0600

    server publish.sh

--- minecraft-launcher-server/publish.sh
+++ minecraft-launcher-server/publish.sh
@@ -1,7 +1,1 @@
-dotnet publish -c Release -o bin/publish \
-  -p:PublishSingleFile=true \
-  -p:PublishTrimmed=true \
-  -p:TrimMode=partial \
-  -p:EnableCompressionInSingleFile=true \
-  -p:InvariantGlobalization=true \
-  -p:DebugType=embedded
+dotnet publish -c Release -o bin/publish
